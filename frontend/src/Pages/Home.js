426adf6237a862d7b6e20422e08d79a4fcdbbcb8
Cesar Arnouk <dev.cesararnouk@gmail.com>
2023-03-17 19:20:45 -0400

    started react

---
+++ frontend/src/Pages/Home.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import NavBar from '../Components/NavBar';
+
+const Home = () => {
+    return (
+        <NavBar/>
+    );
+}
+
+export default Home;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,14 +1,17 @@
-import './mui.min.css';
 import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
-import App from './App';
-import './index.css';
-import * as serviceWorker from './serviceWorker';
+import ReactDOM from 'react-dom/client';
+import Home from './Pages/Home';
+import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-ReactDOM.render(<BrowserRouter><App /></BrowserRouter>, document.getElementById('root'));
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <Home />
+  </React.StrictMode>
+);
 
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
