71158158130ded176fed82e039c8189c5e73d546
rodolphebaladi <rodolphebaladi@gmail.com>
2023-03-17 21:46:53 -0400

    fixed navbar

--- frontend/src/Components/NavBar.js
+++ frontend/src/Components/NavBar.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
@@ -7,18 +8,17 @@ const NavBar = () => {
     return (
         <Navbar bg="light" expand="lg">
             <Container>
-                <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+                <Navbar.Brand href="/Home">Home</Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="me-auto">
-                        <Nav.Link href="#home">Home</Nav.Link>
-                        <Nav.Link href="#link">Link</Nav.Link>
-                        <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                        <Nav.Link href="/Explore">Explore</Nav.Link>
+                        <Nav.Link href="/Inference">Inference</Nav.Link>
+                        <NavDropdown title="Profile" id="basic-nav-dropdown">
+                            <NavDropdown.Item href="/Profile">Visit Profile</NavDropdown.Item>
                             <NavDropdown.Divider />
-                            <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+                            {/* TODO: Implement logout and link to here */}
+                            <NavDropdown.Item href="#SIGNOUT">Sign out</NavDropdown.Item>
                         </NavDropdown>
                     </Nav>
                 </Navbar.Collapse>
