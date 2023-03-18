72276f6547100760eedd07f8917a88654a0c1f78
rodolphebaladi <rodolphebaladi@gmail.com>
2023-03-18 18:16:35 -0400

    Not found page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import Home from './Pages/Home';
 import Inference from './Pages/Inference';
 import Explore from './Pages/Explore';
 import Profile from './Pages/Profile';
+import NotFound from './Pages/NotFound';
 import { BrowserRouter as Router, Switch, Route} from "react-router-dom";
 
 function App() {
@@ -14,9 +15,10 @@ function App() {
         <Route path="/Inference"><Inference /></Route>
         <Route path="/Explore"><Explore /></Route>
         <Route path="/Profile"><Profile /></Route>
+        <Route path="*"><NotFound /></Route>
       </Switch>
     </Router>
-  );
+  )
 }
 
 export default App;
