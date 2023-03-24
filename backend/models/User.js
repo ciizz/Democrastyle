6041bcf61041842ead2b249838c76c3d58f007b8
rodolphebaladi <rodolphebaladi@gmail.com>
2023-03-24 00:59:23 -0400

    unit test module all tests pass

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,5 +1,8 @@
 class User {
   constructor(username, firstName, lastName, email, profilePicture) {
+    if (!email || !username || !firstName || !lastName || !profilePicture) {
+      throw new Error('Email, username, first name, last name and profile picture are required');
+    }
     this.email = email;
     this.username = username;
     this.firstName = firstName;
