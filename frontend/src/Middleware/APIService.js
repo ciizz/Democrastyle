3745e64d0758e45548079223d586388f4d2f8678
rodolphebaladi <rodolphebaladi@gmail.com>
2023-04-01 18:30:31 -0400

    display count on home page

--- frontend/src/Middleware/APIService.js
+++ frontend/src/Middleware/APIService.js
@@ -73,6 +73,11 @@ class APIService {
         let buffer = await new Response(response.data).arrayBuffer();
         return new Uint8Array(buffer);
     }
+
+    static async getRequestCounts() {
+        const response = await axios.get(DEMOCRASTYLE_API_URL + 'images/request_counts');
+        return response.data;
+    }
 }
 
 export default APIService;
--- frontend/src/Pages/Home.js
+++ frontend/src/Pages/Home.js
@@ -1,11 +1,20 @@
-import React from 'react';
-import { Container, Button } from 'react-bootstrap';
+import React, { useState, useEffect } from 'react';
+import { Container, Button, Table } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import { useAuth } from '../Contexts/AuthContext';
+import APIService from '../Middleware/APIService';
 
 function Home() {
-
+  const [requestCount, setRequestCount] = useState(null);
   const { currentUser } = useAuth();
+
+  useEffect(() => {
+    async function fetchRequestCount() {
+      const response = await APIService.getRequestCounts();
+      setRequestCount(response);
+    }
+    fetchRequestCount();
+  }, []);
 
   return (
     <Container className="d-flex flex-column justify-content-center align-items-center">
@@ -24,6 +33,24 @@ function Home() {
       <section className="text-center my-5" style={{ fontStyle: 'italic', color: 'gray' }}>
         <h2>About Us</h2>
         <p className="lead ">We are a group a students from McGill University. Our platform Democrastyle provides image Neural Style Transfer (NST) to its users. The aim is to democratize the use of machine learning tools, making them accessible to everyone, regardless of technical expertise. The platform is designed to provide high-quality style transfer tools for free, by leveraging performant pre-trained NST models. The motivation behind the platform is to contribute to making machine learning tools more accessible for everyday tasks. Additionally, the platform will foster an online community of users and artists.</p>
+        {requestCount && (
+          <Table striped bordered hover>
+            <thead>
+              <tr>
+                <th>Country</th>
+                <th>Count</th>
+              </tr>
+            </thead>
+            <tbody>
+              {Object.entries(requestCount).map(([country, count]) => (
+                <tr key={country}>
+                  <td>{country}</td>
+                  <td>{count}</td>
+                </tr>
+              ))}
+            </tbody>
+          </Table>
+        )}
       </section>
     </Container>
   );
